1677ab5270c3d6721c8d7ff2a5ebe50763507d6f
Joel Peralta <getajoel@gmail.com>
2018-03-31 21:23:27 +0200

    Added_cpu_info

--- monitorize.sh
+++ monitorize.sh
@@ -2,10 +2,11 @@
 
 while IFS='' read -r line || [[ -n "$line" ]]; do
     echo "Container name: $line"
-    echo "    Image: $(docker inspect -f='{{json .Config.Image}}' $line)"
-    network=$(docker inspect -f='{{json .HostConfig.NetworkMode}}' $line | sed -e 's/^"//' -e 's/"$//')
-    ip=$(docker inspect -f='{{json .NetworkSettings.Networks.'"$network"'.IPAddress}}' $line | sed -e 's/^"//' -e 's/"$//')
+    echo "    Image: $(sudo docker inspect -f='{{json .Config.Image}}' $line)"
+    network=$(sudo docker inspect -f='{{json .HostConfig.NetworkMode}}' $line | sed -e 's/^"//' -e 's/"$//')
+    ip=$(sudo docker inspect -f='{{json .NetworkSettings.Networks.'"$network"'.IPAddress}}' $line | sed -e 's/^"//' -e 's/"$//')
     echo "    IP: $ip"
     echo "    Docker network: $network"
-    #echo "$(sshpass -p docker ssh docker@$ip cat /proc/cpuinfo)"
+    echo "    $(sshpass -p docker ssh docker@$ip cat /proc/cpuinfo | grep -m 1 'model name')"
+    echo "    $(sshpass -p docker ssh docker@$ip cat /proc/cpuinfo | grep -m 1 'cpu cores')"
 done < "$1"
